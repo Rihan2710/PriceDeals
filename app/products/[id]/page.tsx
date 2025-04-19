da7c6fa3bdcfb373794574f206fe864068e1a0d8
Rihan Nardekar <99471574+Rihan2710@users.noreply.github.com>
2025-04-19 09:31:58 +0530

    Update page.tsx

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,23 +1,21 @@
 import Modal from "@/components/Modal";
 import PriceInfoCard from "@/components/PriceInfoCard";
 import ProductCard from "@/components/ProductCard";
-import { getProductById, getSimilarProducts } from "@/lib/actions";
+import { getProductById, getSimilarProducts } from "@/lib/actions"
 import { formatNumber } from "@/lib/utils";
 import { Product } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-export default async function ProductDetails({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = params;
+type Props = {
+  params: { id: string }
+}
 
+const ProductDetails = async ({ params: { id } }: Props) => {
   const product: Product = await getProductById(id);
 
-  if (!product) redirect("/");
+  if(!product) redirect('/')
 
   const similarProducts = await getSimilarProducts(id);
 
@@ -25,7 +23,7 @@ export default async function ProductDetails({
     <div className="product-container">
       <div className="flex gap-28 xl:flex-row flex-col">
         <div className="product-image">
-          <Image
+          <Image 
             src={product.image}
             alt={product.title}
             width={580}
@@ -52,19 +50,20 @@ export default async function ProductDetails({
 
             <div className="flex items-center gap-3">
               <div className="product-hearts">
-                <Image
+                <Image 
                   src="/assets/icons/red-heart.svg"
                   alt="heart"
                   width={20}
                   height={20}
                 />
+
                 <p className="text-base font-semibold text-[#D46F77]">
                   {product.reviewsCount}
                 </p>
               </div>
 
               <div className="p-2 bg-white-200 rounded-10">
-                <Image
+                <Image 
                   src="/assets/icons/bookmark.svg"
                   alt="bookmark"
                   width={20}
@@ -73,7 +72,7 @@ export default async function ProductDetails({
               </div>
 
               <div className="p-2 bg-white-200 rounded-10">
-                <Image
+                <Image 
                   src="/assets/icons/share.svg"
                   alt="share"
                   width={20}
@@ -96,19 +95,19 @@ export default async function ProductDetails({
             <div className="flex flex-col gap-4">
               <div className="flex gap-3">
                 <div className="product-stars">
-                  <Image
+                  <Image 
                     src="/assets/icons/star.svg"
                     alt="star"
                     width={16}
                     height={16}
                   />
                   <p className="text-sm text-primary-orange font-semibold">
-                    {product.stars || "25"}
+                    {product.stars || '25'}
                   </p>
                 </div>
 
                 <div className="product-reviews">
-                  <Image
+                  <Image 
                     src="/assets/icons/comment.svg"
                     alt="comment"
                     width={16}
@@ -122,29 +121,29 @@ export default async function ProductDetails({
 
               <p className="text-sm text-black opacity-50">
                 <span className="text-primary-green font-semibold">93% </span> of
-                buyers have recommended this.
+                buyers have recommeded this.
               </p>
             </div>
           </div>
 
           <div className="my-7 flex flex-col gap-5">
             <div className="flex gap-5 flex-wrap">
-              <PriceInfoCard
+              <PriceInfoCard 
                 title="Current Price"
                 iconSrc="/assets/icons/price-tag.svg"
                 value={`${product.currency} ${formatNumber(product.currentPrice)}`}
               />
-              <PriceInfoCard
+              <PriceInfoCard 
                 title="Average Price"
                 iconSrc="/assets/icons/chart.svg"
                 value={`${product.currency} ${formatNumber(product.averagePrice)}`}
               />
-              <PriceInfoCard
+              <PriceInfoCard 
                 title="Highest Price"
                 iconSrc="/assets/icons/arrow-up.svg"
                 value={`${product.currency} ${formatNumber(product.highestPrice)}`}
               />
-              <PriceInfoCard
+              <PriceInfoCard 
                 title="Lowest Price"
                 iconSrc="/assets/icons/arrow-down.svg"
                 value={`${product.currency} ${formatNumber(product.lowestPrice)}`}
@@ -163,16 +162,12 @@ export default async function ProductDetails({
           </h3>
 
           <div className="flex flex-col gap-4">
-            {product?.description?.split("\n").map((line, index) => (
-              <p key={index} className="text-base text-black opacity-80">
-                {line}
-              </p>
-            ))}
+            {product?.description?.split('\n')}
           </div>
         </div>
 
         <button className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]">
-          <Image
+          <Image 
             src="/assets/icons/bag.svg"
             alt="check"
             width={22}
@@ -185,7 +180,7 @@ export default async function ProductDetails({
         </button>
       </div>
 
-      {similarProducts && similarProducts.length > 0 && (
+      {similarProducts && similarProducts?.length > 0 && (
         <div className="py-14 flex flex-col gap-2 w-full">
           <p className="section-text">Similar Products</p>
 
@@ -197,5 +192,7 @@ export default async function ProductDetails({
         </div>
       )}
     </div>
-  );
+  )
 }
+
+export default ProductDetails
