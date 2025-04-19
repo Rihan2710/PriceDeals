5b6f41963aa1a489ba28fa663f399c646d48159e
Rihan Nardekar <99471574+Rihan2710@users.noreply.github.com>
2025-04-19 09:19:20 +0530

    Update page.tsx

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,21 +1,19 @@
-import Modal from "@/components/Modal";
-import PriceInfoCard from "@/components/PriceInfoCard";
-import ProductCard from "@/components/ProductCard";
-import { getProductById, getSimilarProducts } from "@/lib/actions"
+import { getProductById, getSimilarProducts } from "@/lib/actions";
 import { formatNumber } from "@/lib/utils";
 import { Product } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { notFound } from "next/navigation";
+import Modal from "@/components/Modal";
+import PriceInfoCard from "@/components/PriceInfoCard";
+import ProductCard from "@/components/ProductCard";
 
-// Remove explicit Props type
 const ProductDetails = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
 
   const product: Product = await getProductById(id);
+  if (!product) redirect('/');
 
-  if (!product) notFound(); // or redirect('/')
 
   const similarProducts = await getSimilarProducts(id);
 
